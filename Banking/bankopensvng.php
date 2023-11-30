723433215a3f94e197b01ccc06b829d233a0eb33
Sharmila <sharmilaguna27@gmail.com>
2023-11-30 13:03:08 +0530

    Update bankopensvng.php

--- Banking/bankopensvng.php
+++ Banking/bankopensvng.php
@@ -3,130 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Open Savings Account</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-    <style>
-        .nav{
-            width:100%;
-            height:80px;
-            border:2px solid black;
-            box-shadow: 3px 3px 15px rgb(179, 140, 140);
-            font-family:Caprasimo;
-            background-color: #004c85;
-            color:white;
-        }
-        .navdown{
-            width:100%;
-            height:170px;
-            border:2px solid #0063B2;
-            background-image: linear-gradient(to right, #024277, navy, blue, #024277, #759fc2, navy);
-            /* background-color: #0063b0; */
-            color:white;
-            font-weight:bolder;
-            font-size:35px;
-            letter-spacing: 2px;
-            display:flex;
-        }
-        .navdown p{
-           padding-left:100px;
-           position: relative;
-           bottom:10px;
-        }
-        .agree{
-            color:navy;
-            text-align: center;
-        }
-        .sub{
-            background-image: linear-gradient(to right, green, rgb(121, 236, 121), green);
-            padding:15px;
-            border-radius: 25px;
-            font-size:large;
-            color:white;
-            border-color:green;
-        }
-        .sub:hover{
-            transform: scale(1.1, 1.1);
-        }
-        .atlast{
-            background-color: rgb(238, 234, 230);
-            height: 500px;
-            width:1000px;
-            padding:20px;
-            margin-left: 220px;
-            color:navy;
-            box-shadow: 2px 2px 10px 3px grey;
-        }
-        .atlastdiv{
-            width:950px;
-            height:350px;
-            /* border:2px solid black; */
-            display: flex;
-            flex-wrap: wrap;
-            padding:20px;
-            text-align: center;
-            justify-content: center;
-        }
-        .atlastdiv div{
-            width:400px; height:18;
-            border:2px dotted navy;
-            margin:20px;
-            border-radius:20px;
-            text-align: center;
-            padding-top:30px;
-        }
-        .atlastdiv div:hover{
-            transform: scale(1, 1.2);
-            background-color: lightblue;
-            box-shadow: inset 8px 8px 15px 15px rgb(111, 215, 223);
-        }
-        tel{
-            border:2px solid black;
-            background-color: white;
-        }
-        .footer{
-            background-color: #004c85;
-            color:white;
-            font-weight: bolder;
-            font-size: large;
-            width: 100%;
-            height:30px;
-            padding:10px;
-            position:sticky;
-            bottom:0px;
-            display: flex;
-            align-items: center;
-        }
-        
-        fieldset{
-            width:800px;
-            margin-left:320px;
-            background-image: linear-gradient(to top right, rgb(133, 194, 214), rgb(93, 193, 226), rgb(41, 134, 196));
-            box-shadow: 3px 3px 25px #0063B2;
-            border-color: #004c85;
-            font-size: 20px;
-            font-weight: 900;
-        }
-        small{
-            color:rgb(236, 234, 234);
-            font-weight:200;
-        }
-              
-    </style>
+    <title>Savings Account</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel='stylesheet' href='styling.css'>
 </head>
-<body bgcolor="#95B9C7">
-    <div class="nav">
-        <img src="hdfc.png" height="50px" width="200px" style="position:relative; top:15px; left:120px;">
-        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 30px; color:white; position:relative; left:1120px;">home</i></a>
-    </div>
-    <div class="navdown">
-        <img src="hdfc lady2.jpg" height="170px" width="550px">
-        <p  style="text-shadow: 5px 3px 3px rgb(201, 36, 36);">We have upgraded.<br>You should too.<br>
-        <span style="font-size:28px; font-weight:200;">Open your Account NOW!</span></p>
-    </div>
-    <center><h1>WELCOME TO HDFC BANK</h1></center>
-
-    <form name="testing" onsubmit="return checks()" action="savingsdatabase.php" method="post">
+<body>
+<div id='navi'>
+        <div style='background-color:whitesmoke'>
+            <img class='img-fluid' src="hdfclogo.webp" height="130px" width="130px">
+        </div>
+        <h1>HDFC BANK</h1>
+        <div>
+        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 35px; color:white;">home</i></a>
+        <a href="bankcust.php"><i class="material-icons" style="font-size: 35px; color:white">account_circle</i></a></div>
+</div>
+<div id='navdown'>        
+        <img class='img-fluid' src="hdfc lady2.jpg" height="70px" width="400px">  
+        <div style="text-shadow: 5px 3px 3px rgb(201, 36, 36); line-height:'1.5'; padding:10px;">      
+        <p>We have upgraded.  You should too.</p>
+        <p style="font-size:20px; font-weight:500;">Open your Account NOW!</p></div>
+</div>
+<center>
+<form name="testing" onsubmit="return checks()" action="savingsdatabase.php" method="post">
         <fieldset>
             <legend style="color:navy; font-weight:bolder; font-size: 20px;">
                 NEW REGISTRATION
@@ -149,10 +48,10 @@
             <td><input type="file" name="aadhar" accept=".jpg, .pdf"></td></tr>
             <tr><td>UPLOAD PAN CARD<br><small>(only pdf format is allowed / should not be more than 100KB)</small></td><td></td>
             <td><input type="file" name="pan"  accept=".jpg, .pdf"></td></tr>
-            <tr><td>UPLOAD YOUR PHOTO<br><small>(jpeg, jpg, png / should not be more than 20KB/ not taken before 6months from current date)</small></td><td></td>
-            <td><input type="file" name="photo"  accept=".jpeg, .jpg, .png"></td></tr>
-            <tr><td>UPLOAD YOUR SIGNATURE<br><small>(jpeg, jpg, png / should not be more than 20KB)</small></td><td></td>
-             <td><input type="file" name="sign" accept=".jpeg, .jpg, .png"></td></tr>
+            <tr><td>UPLOAD YOUR PHOTO<br><small>(jpg, png / should not be more than 20KB/ not taken before 6months from current date)</small></td><td></td>
+            <td><input type="file" name="photo"  accept=".jpg, .png"></td></tr>
+            <tr><td>UPLOAD YOUR SIGNATURE<br><small>(jpg, png / should not be more than 20KB)</small></td><td></td>
+             <td><input type="file" name="sign" accept=".jpg, .png"></td></tr>
            <tr><td>AADHAR LINKED MOBILE NUMBER</td><td></td> <td><tel>+91</tel>&nbsp<input type="number" name="numb"></td></tr>
            <tr><td colspan="2"><small>(We will be sending you an OTP to this number, please keep it handy)</small></td></tr>
         </table>
@@ -162,19 +61,16 @@
             <input type="submit" value="Submit and Get OTP >>" class="sub" >
         </center>
         </fieldset>
-
-        <h4 class="agree">By submitting, I confirm that I am an Indian above 18 years of age, residing in <br>
+        <br>
+        <h6 style='color:navy'>By submitting, I confirm that I am an Indian above 18 years of age, residing in <br>
             India and have read & agreed to <span style="font-weight: 200;"><u>HDFC Bank Privacy Policy</u></span> and <span style="font-weight: 200;"><u>T&C</u></span>. I agree to<br> 
             receive calls, SMS & WhatsApp messages from HDFC Bank, and this consent <br>
-            overrides any registration for DNC / NDNC.</h4>
+            overrides any registration for DNC / NDNC.</h6>
     </form>
-    
     <div class="atlast">
         <h3>Open your Savings A/c instantly</h3>
-
-           <h3>With Aadhaar, open your account instantly, Online.</h3> 
-            
-            <h3>To open an account you should be:</h3>
+        <h3>With Aadhaar, open your account instantly, Online.</h3>             
+        <h3>To open an account you should be:</h3>
         <div class="atlastdiv">
         <div><i class="material-icons" style="font-size:50px;">event</i><br>
         18 years and above</div>        
@@ -186,12 +82,9 @@
             Have original PAN Card</div>
         </div>
     </div>
-    <br>
-    <div class="footer">
-        @copyright HDFC Bank Ltd.
-    </div>
-   <!-- jscript -->
-   <script>
+</center>
+
+<script>
     function checks(){
     document.getElementById('other').value;
     let newnumb=document.testing.numb.value;
@@ -204,7 +97,6 @@
     let photo=document.testing.photo.value;
     let sign=document.testing.sign.value;
     let pan=document.testing.pan.value;
-    // let validatemail = /^(([a-zA-Z0-9\._#$*]+)@([a-zA-Z]+).([a-zA-Z]+))$/;
         
     if(other.checked){
         alert("If you are not an Indian, you are not eligible to open an account in HDFC Bank!");
@@ -258,12 +150,7 @@
         alert("Enter your aadhar linked mobile number, it should not be empty!");
         return false;
     }
-    // else if(!validatemail.test(mailcheck)){
-    //     alert("Please check your email address. Seems not valid!");
-    //     return false;
-    // }
-    // document.write("Thank you! Your form is submitted successfully and you will be getting an OTP to your aadhar linked mobile number.");
-        alert("Thank you! Your form is submitted successfully and you will be getting an OTP to your aadhar linked mobile number.");
+    alert("Thank you! Your form is submitted successfully and you will be getting an OTP to your aadhar linked mobile number.");
 }
    </script>
 </body>
