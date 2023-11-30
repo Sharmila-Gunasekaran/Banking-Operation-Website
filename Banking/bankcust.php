5b66d926d973530f20ef24ab5c1c78a25fbe027e
Sharmila <sharmilaguna27@gmail.com>
2023-11-30 13:01:48 +0530

    Update bankcust.php

--- Banking/bankcust.php
+++ Banking/bankcust.php
@@ -3,107 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bank Customer page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
+    <title>Existing customer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine|Roboto|Caprasimo|Abril Fatface">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel='stylesheet' href='styling.css'>
     <style>
-        .nav{
-            width:100%;
-            height:80px;
-            border:2px solid black;
-            box-shadow: 3px 3px 15px rgb(179, 140, 140);
-            font-family:Caprasimo;
-            background-color: #004c85;
-            color:white;
-        }
-        .navdown{
-            width:100%;
-            height:170px;
-            border:2px solid #0063B2;
-            /* background-color: #0063b0; */
-            background-image: linear-gradient(to right, #024277, navy, blue, #024277, #759fc2, navy);
-            color:white;
-            font-weight:bolder;
-            font-size:35px;
-            letter-spacing: 2px;
-            display:flex;
-        }
-        .navdown p{
-           padding-left:100px;
-           position: relative;
-           bottom:10px;
+        fieldset input{
+            width:80%;
         }
         .sub{
             background-image: linear-gradient(to right, green, rgb(121, 236, 121), green);
-            padding:15px 30px;
             border-radius: 25px;
+            padding:5px 10px;
             font-size:large;
             color:white;
             border-color:green;
         }
-        .sub:hover{
-            transform: scale(1.1, 1.1);
-        }
-        .maindiv{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width:800px;
-            height:450px;
-            margin-left:320px;
-            background-image: linear-gradient(to top right, blue, lavender, rgb(133, 194, 214), rgb(93, 193, 226), rgb(41, 134, 196), navy);
-            box-shadow: 3px 3px 25px #0063B2;
-            border-color: #004c85;
-            font-size: 35px;
-            font-weight: 900;
-            border-color:2px solid rgb(0, 26, 255);
-        }
-        .footer{
-            background-color: #004c85;
-            color:white;
-            font-weight: bolder;
-            font-size: large;
-            width: 100%;
-            height:30px;
-            padding:10px;
-            display: flex;
-            align-items: center;
-        }
     </style>
 </head>
-<body bgcolor="#95B9C7">
-    <div class="nav">
-        <img src="hdfc.png" height="50px" width="200px" style="position:relative; top:15px; left:120px;">
-        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 30px; color:white; position:relative; left:1120px;">home</i></a>
-    </div>
-    <div class="navdown">
-        <img src="hdfc lady2.jpg" height="170px" width="550px">
-        <p style="text-shadow: 5px 3px 3px rgb(201, 36, 36);">We have upgraded.<br>You should too.<br>
-        <span style="font-size:28px; font-weight:200;">Open your Account NOW!</span></p>
-    </div>
-    <br>
-    
-    <div class="maindiv">
+<body>
+<div id='navi'>
+        <div style='background-color:whitesmoke'>
+            <img class='img-fluid' src="hdfclogo.webp" height="130px" width="130px">
+        </div>
+        <h1>HDFC BANK</h1>
+        <div>
+        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 35px; color:white;">home</i></a>
+        <a href="bankcust.php"><i class="material-icons" style="font-size: 35px; color:white">account_circle</i></a></div>
+</div>
+<div id='navdown'>        
+        <img class='img-fluid' src="hdfc lady2.jpg" height="70px" width="400px">  
+        <div style="text-shadow: 5px 3px 3px rgb(201, 36, 36); line-height:'1.5'; padding:10px;">      
+        <p>We have upgraded.  You should too.</p>
+        <p style="font-size:20px; font-weight:500;">Open your Account NOW!</p></div>
+</div>
+<div>
+<center>
         <form name="myform" onsubmit="return validation()"  method="post" action="bankpin.php">
+        <fieldset>
             <table>
                 <tr><td>Account number:</td><td></td><td><input type="text" autofocus name="acc" style="padding:10px 20px; border-radius:10px;"></td></tr>
                 <tr><td>Account Holder Name:</td><td></td><td><input type="text" Placeholder="Your First name" name="firstname" style="padding:10px 20px; border-radius:10px;"></td>
                 <td><input type="text" Placeholder="Your Last name" name="lastname" style="padding:10px 20px; border-radius:10px;"></td></tr>
                 <tr><td>IFSC:</td><td></td><td><input type="text" value="HDFC00356" readonly name="ifsc" style="padding:10px 20px; border-radius:10px;"></td></tr>
             </table>
-            <br>
-            <center>               
-               <br>
-               <input type="reset" value="Clear" class="sub" style="padding-left: 30px; padding-right: 30px;">&nbsp; &nbsp;
-               <input type="submit" value="SUBMIT" class="sub">
+            <br><br>
+               <button type="reset" class="sub">CLEAR</button>&nbsp; &nbsp;
+               <button type="submit" class="sub">SUBMIT</button>
             </center>
-
+</fieldset>
         </form>
-    </div>
-    <br>
-    <div class="footer">
-        @copyright HDFC Bank Ltd.
-    </div>
+</div>
     <script>
         function validation(){
             let name=document.myform.acc.value;
