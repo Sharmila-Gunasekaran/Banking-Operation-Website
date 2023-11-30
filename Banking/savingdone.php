90aacc4c5d254bc3fd5f38d68c0b28c8c956f37f
Sharmila <sharmilaguna27@gmail.com>
2023-11-30 13:03:50 +0530

    Update savingdone.php

--- Banking/savingdone.php
+++ Banking/savingdone.php
@@ -1,119 +1,88 @@
 <?php    
  $pinnum = $_POST['pinno'];
- $nummo = $_POST['numo'];
-    
+ $nummo = $_POST['numo'];    
     $conn=new mysqli("localhost", "root", "", "test" );    
     // if ($conn->connect_error) {
     //     die("Connection failed: " . $conn->connect_error);
     // }
     // else{
     //     echo "connected";
-    // }
-     // 
+    // }     
 
-     $var2="SELECT MAX(Accountnumber) FROM Savings";
+     $var2="SELECT MAX(Accountnumber) FROM Savingss";
     $query1=$conn->query($var2);
 
     $element=mysqli_fetch_array($query1);
     $maxAccountNumber=$element[0];
     
-    $var1="UPDATE Savings SET PINnumber = '$pinnum' WHERE Accountnumber = '$nummo'";
+    $var1="UPDATE Savingss SET PINnumber = '$pinnum' WHERE Accountnumber = '$nummo'";
     $query2 = $conn->query($var1);     
-    ?> 
-
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BankOTP Page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
+    <title>Savings account created</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine|Roboto|Caprasimo|Abril Fatface">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel='stylesheet' href='styling.css'>
     <style>
-        .nav{
-            width:100%;
-            height:80px;
-            border:2px solid black;
-            box-shadow: 3px 3px 15px rgb(179, 140, 140);
-            font-family:Caprasimo;
-            background-color: #004c85;
-            color:white;
-        }
-        .navdown{
-            width:100%;
-            height:170px;
-            border:2px solid #0063B2;
-            /* background-color: #0063b0; */
-            background-image: linear-gradient(to right, #024277, navy, blue, #024277, #759fc2, navy);
-            color:white;
-            font-weight:bolder;
-            font-size:35px;
-            letter-spacing: 2px;
-            display:flex;
-        }
-        .navdown p{
-           padding-left:100px;
-           position: relative;
-           bottom:10px;
-        }
         .firsth1{
           font-size:45px;
-          text-shadow: 3px 3px 5px  blue;
-          animation: blinkme 1s infinite;
+          animation: blinkme 1s 1;
         }
         @keyframes blinkme{
           0%{
-               transform: scale(0.9, 0.9);
-               text-shadow: 3px 3px 10px  aqua;
-               
+               transform: scale(0.9, 0.9);               
           }
           50%{              
-               text-shadow: 3px 3px 10px blue;
                transform: scale(1, 1);
           }
           100%{
                transform: scale(0.9, 0.9);
-               text-shadow: 3px 3px 10px  navy;
           }
         }
         .secondh1{
           font-size:45px;
-          text-shadow: 3px 3px 5px  blue;
-          animation: blinkmee 1s infinite;
+          animation: blinkmee 1s 1;
         }
         @keyframes blinkmee{
           0%{
-               text-shadow: 3px 3px 10px blue;
-               transform: scale(1, 1);             
-               
+               transform: scale(1, 1);  
           }
           50%{              
-               transform: scale(0.9, 0.9);
-               text-shadow: 3px 3px 10px  navy;
+               transform: scale(0.9, 0.9);               
           }
           100%{
-               transform: scale(1, 1);
-               text-shadow: 3px 3px 10px  aqua;
+               transform: scale(1, 1);               
           }
         }
         a{
           text-decoration-line:none;
           color:white;
         }
-        </style>
-
-        <body bgcolor="#95B9C7">
-    <div class="nav">
-        <img src="hdfc.png" height="50px" width="200px" style="position:relative; top:15px; left:120px;">
-        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 30px; color:white; position:relative; left:1120px;">home</i></a>
-    </div>
-    <div class="navdown">
-        <img src="hdfc lady2.jpg" height="170px" width="550px">
-        <p  style="text-shadow: 5px 3px 3px rgb(201, 36, 36);">We have upgraded.<br>You should too.<br>
-        <span style="font-size:28px; font-weight:200;">Open your Account NOW!</span></p>
-    </div>
-    <br><br>
-    <center><h1 class="firsth1">Thank You!</h1>
-     <h1  class="secondh1">You have opened your Savings Account in HDFC Bank!</h1></center>
-    </body>
-    </html>
+    </style>
+</head>
+<body>
+<div id='navi'>
+        <div style='background-color:whitesmoke'>
+            <img class='img-fluid' src="hdfclogo.webp" height="130px" width="130px">
+        </div>
+        <h1>HDFC BANK</h1>
+        <div>
+        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 35px; color:white;">home</i></a>
+        <a href="bankcust.php"><i class="material-icons" style="font-size: 35px; color:white">account_circle</i></a></div>
+</div>
+<div id='navdown'>        
+        <img class='img-fluid' src="hdfc lady2.jpg" height="70px" width="400px">  
+        <div style="text-shadow: 5px 3px 3px rgb(201, 36, 36); line-height:'1.5'; padding:10px;">      
+        <p>We have upgraded.  You should too.</p>
+        <p style="font-size:20px; font-weight:500;">Open your Account NOW!</p></div>
+</div>
+<br><br>
+    <center><h2 class="firsth1">Thank You!</h2>
+     <h2  class="secondh1">You have opened your Savings Account in HDFC Bank!</h2></center>
+</body>
+</html>
