a9cc0db75daee3fe1e70aca39d25fb687e90175a
Sharmila <sharmilaguna27@gmail.com>
2023-11-30 13:02:27 +0530

    Update bankexitpage.php

--- Banking/bankexitpage.php
+++ Banking/bankexitpage.php
@@ -3,80 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bank Exit page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-        <style>
-            .div1{
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                font-size:50px;
-                font-weight: 900;
-                color:aqua;
-                text-shadow: 5px 4px 6px rgb(0, 255, 64);
-                text-align: center;
-                height:600px;
-            }
-            .nav{
-            width:100%;
-            height:80px;
-            border:2px solid black;
-            box-shadow: 3px 3px 15px rgb(179, 140, 140);
-            font-family:Caprasimo;
-            background-color: #004c85;
-            color:white;
-        }
-            .show{
-                animation-name: showme;                
-                animation-duration: 2s;
-                animation-iteration-count: 1;
-                animation-timing-function: linear;
-                position:relative;
-                bottom: 40px;
-            }
-            @keyframes showme{
-                0%{
-                    transform: scale(1.5, 1.5);
-                }
-                50%{
-                    transform: scale(0.6, 0.6);
-                }
-                100%{
-                    transform: scale(1, 1);
-                }
-            }
-            .footer{
-            background-color: #004c85;
-            color:white;
-            font-weight: bolder;
-            font-size: large;
-            width: 100%;
-            height:30px;
-            padding:10px;
-            display: flex;
-            align-items: center;
-            position:sticky;
-            bottom:0px;
-        }
-        body{
-            background-color: #95B9C7;
-        }
-        </style>
+    <title>Exit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine|Roboto|Caprasimo|Abril Fatface">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel='stylesheet' href='styling.css'>
 </head>
 <body>
-<div class="nav">
-        <img src="hdfc.png" height="50px" width="200px" style="position:relative; top:15px; left:120px;">
-        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 30px; color:white; position:relative; left:1120px;">home</i></a>
-        <a href="bankcust.php"><i class="material-icons" style="font-size: 30px; color:white; position:relative; left:1140px;">account_circle</i></a>
+<div id='navi'>
+        <div style='background-color:whitesmoke'>
+            <img class='img-fluid' src="hdfclogo.webp" height="130px" width="130px">
+        </div>
+        <h1>HDFC BANK</h1>
+        <div>
+        <a href="bankfrontpg.php"><i class="material-icons" style="font-size: 35px; color:white;">home</i></a>
+        <a href="bankcust.php"><i class="material-icons" style="font-size: 35px; color:white">account_circle</i></a></div>
+</div>
+<div id='navdown' style='background-color: #0063b0;'>        
+        <img class='img-fluid' src="hdfc lady2.jpg" height="70px" width="400px">  
+        <div style="text-shadow: 5px 3px 3px rgb(201, 36, 36); line-height:'1.5'; padding:10px;">      
+        <p>We have upgraded.  You should too.</p>
+        <p style="font-size:20px; font-weight:500;">Open your Account NOW!</p></div>
+</div>
+<div style='display:flex; flex-direction:column; justify-content:center;align-items:center;min-height:420px'>
+        <h2 class="show">LOGGED OUT SUCCESSFULLY!</h2>
+        <h2>THANK YOU FOR BANKING WITH <span style="color:navy;">HDFC</span>!</h2>
     </div>
-    <div class="div1">
-        <P class="show">LOGGED OUT SUCCESSFULLY!
-        <br>THANK YOU FOR BANKING WITH <span style="color:rgb(236, 82, 146); text-shadow: 4px 4px 6px rgb(251, 255, 0);">HDFC</span>!</P>
-    </div>
-    <br>
-    <div class="footer">
+<div class="footer">
         @copyright HDFC Bank Ltd.
-    </div>
+</div>
 </body>
 </html>
